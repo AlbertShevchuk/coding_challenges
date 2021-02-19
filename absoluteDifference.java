5cff9e8506c34ae7a0503d2c6125506b2784add2
albertshevchuk <albertshevchuk@gmail.com>
2021-02-19 09:58:12 -0800

    absolute diagnal difference in a given matrix

---
+++ absoluteDifference.java
@@ -0,0 +1,16 @@
+ //Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+ public static int diagonalDifference(List<List<Integer>> arr) {
+        
+      int left = 0, right = 0;
+      
+      for(int i = 0; i < arr.size(); i++ ){
+          right = right + arr.get(i).get(i);  
+          left = left + arr.get(arr.size() - i - 1).get(i);
+          }
+          
+        return Math.abs(Math.abs(right) - Math.abs(left));  
+   
+    }
+	
+	
